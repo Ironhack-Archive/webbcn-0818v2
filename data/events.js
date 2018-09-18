9a504f4f3703ab42184a49664dff95f4b22d6416
Francesca Ropolo <franci.ropolo@gmail.com>
2018-09-18 17:00:57 +0200

    added in events the attendees

--- data/events.js
+++ data/events.js
@@ -1,19 +1,23 @@
 const data = [{
   name: 'Ironbeers',
   type: 'party',
-  date: '2018-09-14'
+  date: '2018-09-14',
+  attendees: ['Axel']
 }, {
   name: 'BBQ',
   type: 'party',
-  date: '2018-08-31'
+  date: '2018-08-31',
+  attendees: []
 }, {
   name: 'Game Show',
   type: 'presentation',
-  date: '2018-09-07'
+  date: '2018-09-07',
+  attendees: ['Barbara', 'Francesca']
 }, {
   name: 'UX-UI',
   type: 'talk',
-  date: '2018-09-12'
+  date: '2018-09-12',
+  attendees: []
 }];
 
 module.exports = data;
